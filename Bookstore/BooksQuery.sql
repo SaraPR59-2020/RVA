c3b95b02d34e1e595e1aec548fc11b9b39df69aa
Moco2669 <moxtheboom004@gmail.com>
2024-05-21 23:01:45 +0200

    setting up database

---
+++ Bookstore/BooksQuery.sql
@@ -0,0 +1,19 @@
+﻿Create table Authors
+(
+	AuthorId int primary key identity,
+	ShortDesc text
+)
+Create table Books
+(
+	BookId int primary key identity,
+	Title text,
+	PublishYear int,
+	AuthorsId int foreign key references Authors(AuthorId),
+	RentedTo varchar(50) foreign key references Members(Username)
+)
+Create table Members
+(
+	Username varchar(50) primary key,
+	FirstName text,
+	LastName text,
+)
---
+++ Bookstore/BookstoreBackend/BookstoreDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+
+namespace BookstoreBackend
+{
+    public class BookstoreDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Member> Members { get; set; }
+        public DbSet<Member> Admins { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder dbModelBuilder)
+        {
+            Database.SetInitializer<BookstoreDbContext>(new CreateDatabaseIfNotExists<BookstoreDbContext>()); // null instead of CreateIfNotExist?
+            base.OnModelCreating(dbModelBuilder);
+        }
+    }
+}
