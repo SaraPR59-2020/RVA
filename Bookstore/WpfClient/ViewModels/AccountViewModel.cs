d246f77b1640d224a465b8f3e5550c35f5f064ac
Sara Stamenkovic <stamenkovic.pr59.2020@uns.ac.rs>
2024-06-04 00:25:56 +0200

    AccountView

---
+++ Bookstore/WpfClient/ViewModels/AccountViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfClient.Misc;
+
+namespace WpfClient.ViewModels
+{
+    internal class AccountViewModel : ViewModelBase
+    {
+        public string FirstNameTextBox { get; set; }
+        public string LastNameTextBox { get; set; }
+
+        public Command SaveCommand { get; set; }
+
+
+
+        public AccountViewModel()
+        {
+            //UserInfo ui = Session.Current.LibraryProxy.GetUserInfo(Session.Current.LoggedInUser);
+            //FirstNameTextBox = ui.FirstName;
+            //LastNameTextBox = ui.LastName;
+
+            //SaveCommand = new Command(SaveAccountChanges);
+        }
+
+        private void SaveAccountChanges()
+        {
+            //Session.Current.LibraryProxy.EditUserInfo(Session.Current.LoggedInUser, FirstNameTextBox, LastNameTextBox);
+            //ClientLogger.Log($"Account {Session.Current.LoggedInUser} successfully edited.", Common.LogLevel.Info);
+        }
+    }
+}
