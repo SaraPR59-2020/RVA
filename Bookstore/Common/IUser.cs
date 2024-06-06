e6e8c26b69df509c90ac9d08b750485e7e87c2cd
Moco2669 <moxtheboom004@gmail.com>
2024-06-06 11:42:01 +0200

    interface segregation

---
+++ Bookstore/Common/IUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Model;
+
+namespace Common
+{
+    public interface IUser
+    {
+        string LogIn(string username, string password);
+        void LogOut(string token);
+        Member GetMemberInfo(string token);
+        bool EditMemberInfo(string firstName, string lastName, string token);
+    }
+}
